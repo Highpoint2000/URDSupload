9161ebdc18c4b52122c3ad0366d5f12f46df8c77
Highpoint2000 <168109804+Highpoint2000@users.noreply.github.com>
2025-01-04 18:11:47 +0100

    Update urds-upload.js

--- URDS-Uploader/urds-upload.js
+++ URDS-Uploader/urds-upload.js
@@ -1,7 +1,7 @@
 (() => {
 ////////////////////////////////////////////////////////////////
 ///                                                          ///
-///  URDS UPLOADER CLIENT SCRIPT FOR FM-DX-WEBSERVER (V1.0)  ///
+///  URDS UPLOADER CLIENT SCRIPT FOR FM-DX-WEBSERVER (V1.0a) ///
 ///                                                          ///
 ///  by Highpoint                last update: 03.01.25       ///
 ///                                                          ///
@@ -15,7 +15,7 @@ const updateInfo = true; // Enable or disable version check
 
 /////////////////////////////////////////////////////////////////
 
-    const plugin_version = '1.0';
+    const plugin_version = '1.0a';
 	const plugin_path = 'https://raw.githubusercontent.com/highpoint2000/URDSupload/';
 	const plugin_JSfile = 'main/URDS-Uploader/urds-upload.js'
 	const plugin_name = 'URDS Uploader';
